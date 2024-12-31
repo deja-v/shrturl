77a80c8f4306c2f1e90169b72d771c214330162f
Devang Jain <devangj870@gmail.com>
2024-12-31 20:47:04 +0530

    remove cors

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 8000;
 //connection
 connectDB();
 
-app.use(cors({
-    origin: 'https://shortly-jade-tau.vercel.app/', 
-    methods: 'GET,POST,PUT,DELETE',
-}));
+// app.use(cors({
+//     origin: 'https://shortly-jade-tau.vercel.app/', 
+//     methods: 'GET,POST,PUT,DELETE',
+// }));
 
 app.use(express.urlencoded({extended:false}))
 
