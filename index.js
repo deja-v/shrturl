0ad662c8b9bfd15a5e8d65978f0533ca8d092be6
Devang Jain <devangj870@gmail.com>
2024-12-31 21:04:07 +0530

    add cors with allowed origins

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ import 'dotenv/config'
 import express from 'express';
 import connectDB from './connection.js';
 import urlRouter from './routes/url.js';
+import cors from "cors"
 const app = express();
 const port = process.env.PORT || 8000;
 
 //connection
 connectDB();
 
-// app.use(cors({
-//     origin: 'https://shortly-jade-tau.vercel.app/', 
-//     methods: 'GET,POST,PUT,DELETE',
-// }));
+const allowedOrigins = [
+    'http://localhost:5173', // Local frontend during development
+    'https://shortly-jade-tau.vercel.app/', // Deployed frontend
+  ];
+  
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: 'GET,POST,PUT,DELETE', 
+    })
+  );
+  
 
 app.use(express.urlencoded({extended:false}))
 
